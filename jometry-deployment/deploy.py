91a361d46755126fa00128387f603b43a0620dc0
TK Focht <tkfocht@alum.mit.edu>
2022-07-29 16:10:04 -0700

    Include content type in deployment copy

--- jometry-deployment/deploy.py
+++ jometry-deployment/deploy.py
@@ -6,12 +6,25 @@ from typing import List
 def get_s3_client():
     return boto3.client('s3')
 
+def get_content_type_for_path(path: Path) -> str:
+    if path.suffix == '.html':
+        return 'text/html'
+    elif path.suffix == '.css':
+        return 'text/css'
+    elif path.suffix == '.js':
+        return 'application/javascript'
+    else:
+        raise BaseException('Unknown suffix ' + path.suffix)
+
 def upload_to_s3(pathlist: List[Path]):
     try:
         s3_client = get_s3_client()
         for path in pathlist:
             response = s3_client.upload_file(str(path), 'j-ometry', str(path.relative_to('site')), \
-                ExtraArgs={'CacheControl': 'max-age=300'})
+                ExtraArgs={
+                    'CacheControl': 'max-age=300',
+                    'ContentType': get_content_type_for_path(path)
+                })
     except ClientError as e:
         print(e)
 
